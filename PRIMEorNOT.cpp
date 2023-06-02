8d5fb0460c9eb2700dfc2eb4fb72a7df17f02d12
DITNO <92884446+DITNO@users.noreply.github.com>
2023-06-02 13:24:25 +0530

    Create PRIMEorNOT.cpp

---
+++ PRIMEorNOT.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool prime(int n){
+	if(n<1){
+		return false;
+	}
+	for(int i=2;i<sqrt(n);i++){
+		if(n%i==0){
+			return false;
+		}
+	}
+	return true;
+}
+int main() {
+	int n;
+	cin>>n;
+	prime(n)?cout<<"Prime":cout<<"Not Prime";
+	return 0;
+}
