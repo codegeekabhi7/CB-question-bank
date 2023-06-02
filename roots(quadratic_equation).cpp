293893c45cf5e03d4044dbbe2757f19f2d89e472
DITNO <92884446+DITNO@users.noreply.github.com>
2023-06-02 10:23:32 +0530

    Update roots(quadratic_equation).cpp

--- roots(quadratic_equation).cpp
+++ roots(quadratic_equation).cpp
@@ -9,14 +9,14 @@ int main() {
 	int root = pow(b,2)-4*a*c;
 	if(root>0){
 		cout<<"Real and Distinct"<<endl;
-			float ans1=(-b(sqrt(root)))/(2*a);
+			float ans1=(-b-(sqrt(root)))/(2*a);
 			float ans2=(-b+(sqrt(root)))/(2*a);
 			cout<<ans1<<" "<<ans2<<endl;
 	}
 	else if(root==0){
-		cout << "Real and Same" << endl;
-        float ans1 = -b / (2 * a);
-        cout << ans1 << " " << ans1 << endl;
+		cout<<"Real and Same"<<endl;
+        float ans = -b/(2*a);
+        cout<<ans<<" "<<ans<<endl;
 	}
 	else{
 		cout<<"Imaginary";
